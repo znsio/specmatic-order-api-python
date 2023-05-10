e372ff0117f4af8e645a7afb7c84afbcc67f58e6
jaydeep <jaydeepk@gmail.com>
2023-05-10 13:47:53 +0530

    Replaced specmatic call with pytest to execute the contract tests in github action. In the pytest, removed code to start the flask app as it is started in the github action itself.

--- test/test_contract_with_pytest.py
+++ test/test_contract_with_pytest.py
@@ -1,22 +1,15 @@
 import pytest
-from specmatic_python.server.flask_server import FlaskServer
 from specmatic_python.specmatic.specmatic_server import SpecmaticServer
-from api import app
 from definitions import ROOT_DIR
 
 host = "127.0.0.1"
 port = 5000
-flask_server = FlaskServer(app, host, port)
 
 
 class TestContract:
-
-    @classmethod
-    def teardown_class(cls):
-        flask_server.stop()
+    pass
 
 
-flask_server.start()
 SpecmaticServer() \
     .with_api_under_test_at(host, port) \
     .with_specmatic_json_at(ROOT_DIR + '/specmatic.json') \
